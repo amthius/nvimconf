26247dd8d72544709ba960a30a670262ca588196
notdan <51867420+dann0001@users.noreply.github.com>
2022-10-19 15:55:59 -0400

    Delete lua directory

--- lua/plugins/bufferline.lua
+++
@@ -1,9 +0,0 @@
-local m = {
-    'akinsho/bufferline.nvim',
-    tag = "v2.*", 
-    requires = 'kyazdani42/nvim-web-devicons',
-    config = function()
-        require("bufferline").setup{highlights = require("catppuccin.groups.integrations.bufferline").get{}}
-    end,
-}
-return m
