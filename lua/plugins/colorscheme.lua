7ba1eecb5167dff8d5ab18d4136c72f22a7d3273
notdan <51867420+dann0001@users.noreply.github.com>
2022-10-19 02:00:29 -0400

    Update colorscheme.lua

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -2,9 +2,33 @@ local m = {
    "catppuccin/nvim",
 	as = "catppuccin",
 	config = function()
-		vim.g.catppuccin_flavour = "mocha" -- latte, frappe, macchiato, mocha
-		require("catppuccin").setup()
-		vim.api.nvim_command "colorscheme catppuccin"
-	end 
+	vim.g.catppuccin_flavour = "mocha" -- latte, frappe, macchiato, mocha
+        require("catppuccin").setup {
+            styles = {
+                comments = {},
+                conditionals = {},
+                loops = {},
+                functions = {},
+                keywords = {},
+                strings = {},
+                variables = {},
+                numbers = {},
+                booleans = {},
+                properties = {},
+                types = {},
+                operators = {},
+            },
+
+            integrations = {
+                cmp = true,
+                nvimtree = true,
+                telescope = true,
+                treesitter = true,
+                bufferline = true,
+            },
+
+        }
+	vim.api.nvim_command "colorscheme catppuccin"
+    end 
 }
 return m
