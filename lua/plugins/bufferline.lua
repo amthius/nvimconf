40d6ef70183673b9ccc21e16b69d730ea42b998a
notdan <51867420+dann0001@users.noreply.github.com>
2022-10-19 02:03:28 -0400

    Update bufferline.lua

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -2,9 +2,8 @@ local m = {
     'akinsho/bufferline.nvim',
     tag = "v2.*", 
     requires = 'kyazdani42/nvim-web-devicons',
-
     config = function()
-        require("bufferline").setup{}
+        require("bufferline").setup{highlights = require("catppuccin.groups.integrations.bufferline").get{}}
     end,
 }
 return m
